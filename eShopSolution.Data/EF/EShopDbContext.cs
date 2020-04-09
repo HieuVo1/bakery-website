210234fe9ac4b97ad71dd0d0d4a7b2fc44ffd73b
Hiếu <17520487@gm.uit.edu.vn>
2020-04-09 20:51:02 +0700

    Add Entity

---
+++ eShopSolution.Data/EF/EShopDbContext.cs
@@ -0,0 +1,25 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    class EShopDbContext : DbContext
+    {
+        public EShopDbContext( DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartProduct> CartProducts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Promotion> Promotions { get; set; }
+    }
+}
---
+++ eShopSolution.Data/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Product
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public decimal Price { set; get; }
+        public decimal OriginalPrice { set; get; }
+        public int Stock { set; get; }
+        public string ProductUrl { set; get; }
+        public DateTime Created_At { set; get; }
+
+    }
+}
