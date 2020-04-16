9d8ad2ff1fbd579bfae7e999c3bddedce873012f
Hiếu <17520487@gm.uit.edu.vn>
2020-04-16 18:33:48 +0700

    Login-Register

---
+++ eShopSolution.Application/System/Users/UserService.cs
@@ -0,0 +1,74 @@
+﻿using eShopSolution.Data.Entities;
+using eShopSolution.Utilities.Exceptions;
+using eShopSolution.ViewModel.System.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.System.Users
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<UserApp> _userManager;
+        private readonly SignInManager<UserApp> _signInManager;
+        private readonly RoleManager<RoleApp> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public UserService(UserManager<UserApp> userManager, SignInManager<UserApp> signInManager, RoleManager<RoleApp> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+        public async Task<string> Authencate(LoginRequest request)
+        {
+            var user = await _userManager.FindByNameAsync(request.UserName);
+            if (user == null) return null;
+            var result = await _signInManager.PasswordSignInAsync(user, request.Passwork, request.RememberMe, true);
+            if (!result.Succeeded) return null;
+
+            var roles = _userManager.GetRolesAsync(user);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Email,user.Email),
+                new Claim(ClaimTypes.Role,string.Join(";",roles)),
+                new Claim(ClaimTypes.Name, request.UserName),
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Tokens:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_configuration["Tokens:Issuer"],
+                _configuration["Tokens:Issuer"],
+                claims,
+                expires: DateTime.Now.AddHours(3),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public async Task<bool> Register(RegisterRequest request)
+        {
+            var user = new UserApp()
+            {
+                Dob = request.Dob,
+                Email = request.Email,
+                PhoneNumber = request.Phone,
+                UserName = request.UserName
+            };
+            var result = await _userManager.CreateAsync(user, request.Passwork);
+            if (result.Succeeded)
+            {
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
